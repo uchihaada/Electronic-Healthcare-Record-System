42f5bbc39cd15e44aaa7e55987849df1daaca55e
Abhijit Das <abhijitadadas@gmail.com>
2024-06-06 21:36:43 +0530

    first commit

---
+++ truffle-config.js
@@ -0,0 +1,32 @@
+
+
+module.exports = {
+  networks: {
+    development: {
+     host: "127.0.0.1",     
+     port: 8545,            
+     network_id: "*",       
+    },
+  },
+
+  // Set default mocha options here, use special reporters etc.
+  mocha: {
+    // timeout: 100000
+  },
+
+  // Configure your compilers
+  compilers: {
+    solc: {
+      version: "0.5.1",    // Fetch exact version from solc-bin (default: truffle's version)
+    }
+  },
+
+  // Truffle DB is currently disabled by default; to enable it, change enabled: false to enabled: true
+  // Note: if you migrated your contracts prior to enabling this field in your Truffle project and want
+  // those previously migrated contracts available in the .db directory, you will need to run the following:
+  // $ truffle migrate --reset --compile-all
+
+  db: {
+    enabled: false
+  }
+};
